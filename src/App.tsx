3fd675d406a7e84d6398db9978bd17acb4080006
Dudás Tamás <ddstms@gmail.com>
2026-03-15 13:25:08 +0100

    Project setup. Install vite, tailwind and shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 function App() {
 return (
 <>
 <h1 className="font-bold underline text-7xl">Hello Word</h1>
+<Button variant="default">Click me</Button>
 </>
 );
 }
