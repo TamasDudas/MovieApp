d6813b3288c09415491a0a8ac2afc1cdd4b68b0c
Dudás Tamás <ddstms@gmail.com>
2026-03-15 13:37:08 +0100

    feat: add next-themes for theme management and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Button } from './components/ui/button';
 
 function App() {
 return (
-<>
+<div className="max-w-7xl mx-auto p-8">
 <h1 className="font-bold underline text-7xl">Hello Word</h1>
 <Button variant="default">Click me</Button>
-</>
+</div>
 );
 }
 
