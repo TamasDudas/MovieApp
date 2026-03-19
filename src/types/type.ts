e6602d0c60cd07a859d3b6f75d5cf634d3029022
Dudás Tamás <ddstms@gmail.com>
2026-03-19 12:26:30 +0100

    feat: add react-router-dom dependency, create Movie interface, and add .env file for API key

---
+++ src/types/type.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+id: number;
+title: string;
+overview: string;
+poster_path: string;
+release_date: string;
+vote_average: number;
+isFavorite: boolean;
+isWatched: boolean;
+isPlanned: boolean;
+}
